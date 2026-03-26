e9ca1b9fd659c52927de277f43d7f81a5dc28393
Ezhkin-Kot <ezkot.tokze@gmail.com>
2026-03-27 00:46:07 +0400

    fix: event loop error

--- src/main.py
+++ src/main.py
@@ -1,10 +1,8 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-
-import asyncio
 
 import uvicorn
 
@@ -13,19 +11,5 @@ from app import create_app
 app = create_app()
 
 
-async def run() -> None:
-    config = uvicorn.Config(
-        "main:app",
-        host="127.0.0.1",
-        port=8000,
-        reload=False,
-    )
-    server = uvicorn.Server(config=config)
-    tasks = (asyncio.create_task(server.serve()),)
-
-    await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
-
-
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(run())
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=False)
